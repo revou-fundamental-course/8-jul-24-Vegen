f5165f8c15a775b9289216ad813f722dd7c46536
Vegen Fendianto <vfendianto@gmail.com>
2024-07-18 16:48:27 +0700

    Mini P

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
-<<<<<<< HEAD
 var usia = document.getElementById("usia");
 var tinggi = document.getElementById("tinggi");
 var berat = document.getElementById("berat");
 var male = document.getElementById("laki-laki");
 var female = document.getElementById("perempuan");
 let resultArea = document.querySelector(".comment");
+
+modalContent = document.querySelector(".modal-content");
+modalText = document.querySelector("#modalText");
+var modal = document.getElementById("myModal");
+var span = document.getElementsByClassName("close")[0];
 
 function calculate(){
  
@@ -47,69 +51,16 @@ function countBmi(){
 resultArea.style.display = "block";
 document.querySelector(".comment").innerHTML = `Kamu <span id="comment">${result}</span>`;
 document.querySelector("#result").innerHTML = bmi.toFixed(2);
+    }
 
+// Ketika memencet (x) saat notif harap isi maka akan ditutup notifnya
+span.onclick = function() {
+  modal.style.display = "none";
 }
 
-
-
-
-
-
-=======
-var usia = document.getElementById("usia");
-var tinggi = document.getElementById("tinggi");
-var berat = document.getElementById("berat");
-var male = document.getElementById("laki-laki");
-var female = document.getElementById("perempuan");
-let resultArea = document.querySelector(".comment");
-
-function calculate(){
- 
-  if(usia.value=='' || tinggi.value=='' || berat.value=='' || (male.checked==false && female.checked==false)){
-    modal.style.display = "block";
-    modalText.innerHTML = `All fields are required!`;
-
-  }else{
-    countBmi();
+// Ketika memencet area diluar notif harap isi maka akan ditutup notifnya
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
-
 }
-
-
-function countBmi(){
-  var p = [usia.value, tinggi.value, berat.value];
-  if(male.checked){
-    p.push("laki-laki");
-  }else if(female.checked){
-    p.push("perempuan");
-  }
-
-  var bmi = Number(p[2])/(Number(p[1])/100*Number(p[1])/100);
-      
-  var result = '';
-  if(bmi<18.5){
-    result = 'Kekurangan berat badan';
-     }else if(18.5<=bmi&&bmi<=24.99){
-    result = 'Normal (ideal)';
-     }else if(25<=bmi&&bmi<=29.99){
-    result = 'Kelebihan berat badan';
-     }else if(30<=bmi&&bmi<=34.99){
-    result = 'Kegemukan (obesitas)';
-     }else if(35<=bmi){
-    result = 'Sangat gemuk';
-     }
-
-
-
-resultArea.style.display = "block";
-document.querySelector(".comment").innerHTML = `Kamu <span id="comment">${result}</span>`;
-document.querySelector("#result").innerHTML = bmi.toFixed(2);
-
-}
-
-
-
-
-
-
->>>>>>> e9a96ba47a5005c9eac46f929eb8e26c71c04c71
